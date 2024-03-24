80f839e8396906207a63c52aa8154b112a8a7c9d
keinuma <nununu.mono@gmail.com>
2024-03-25 00:07:26 +0900

    Dockerへの環境変数設定

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = Number(process.env.PORT) || 3000;
+  const port = Number(process.env.API_PORT) || 3000;
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
